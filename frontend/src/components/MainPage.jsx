cb9212eefe6e3b183eae1b563fb75b132a39132f
Valentina <v.vasileva@nuzhnapomosh.ru>
2023-05-03 17:37:03 +0300

    add redirect

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 
 const MainPage = () => {
+    const user = {};
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!user) {
+            navigate("/login");
+        }
+    }, [user]);
+
     return (
         <div>Hello world Main Page!</div>
     );
