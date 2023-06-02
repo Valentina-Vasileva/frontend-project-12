5ce0796b57bbb510369ee96db1a6cb57a37e4c66
Valentina <v.vasileva@nuzhnapomosh.ru>
2023-06-02 14:40:17 +0300

    i18n global object

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
-import i18next from 'i18next';
+import i18n from 'i18next';
 import { Provider } from 'react-redux';
 import App from './components/App.jsx';
 import resources from './locales/index.js';
@@ -8,7 +8,6 @@ import store from './slices/index.js';
 import AuthProvider from './context/AuthProvider';
 
 const init = async () => {
-  const i18n = i18next.createInstance();
   await i18n
     .use(initReactI18next)
     .init({
